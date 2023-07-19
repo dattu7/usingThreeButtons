54dbcaf24fa0692d3b5dc29372ca4097780e2138
Gundumolu Dattatreya <126433896+dattu7@users.noreply.github.com>
2023-07-19 12:30:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sun()
+{
+    document.getElementById("sunn").src="https://static6.depositphotos.com/1065717/557/i/450/depositphotos_5574617-stock-photo-symbol-of-the-sun.jpg"
+}
+function moon()
+{
+    document.getElementById("sunn").src="https://static.scientificamerican.com/sciam/cache/file/DF8D4FF4-F52A-4315-ABD0429040B25D0F_source.jpg"
+}
+function mars()
+{
+    document.getElementById("sunn").src="https://api.time.com/wp-content/uploads/2022/09/Mars.jpg"
+}
